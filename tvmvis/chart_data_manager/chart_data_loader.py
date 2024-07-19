3dc4fdc0715656ed00525d3d4c3da30ebb8c6043
yc128 <yc128mc@gmail.com>
2024-07-19 17:06:34 +0800

    - Add more parameter types for selection - Update .css

--- tvmvis/chart_data_manager/chart_data_loader.py
+++ tvmvis/chart_data_manager/chart_data_loader.py
@@ -1,4 +1,4 @@
-from django.db.models import ForeignKey, Q
+from django.db.models import ForeignKey, Q, IntegerField, FloatField
 
 from tvmvis.models import Benchmark, Run, TaskResults, TotalResults, TaskGraphResults
 import json
@@ -34,18 +34,28 @@ def get_chart_title_list():
 
 
 def get_all_param_types():
-    fields = TaskResults._meta.get_fields()
+    def extract_numeric_fields(model):
+        fields = model._meta.get_fields()
+        # 提取数字类型字段（整数和浮点数）
+        field_names = [
+            field.name for field in fields
+            if (isinstance(field, IntegerField) or isinstance(field, FloatField))  # 只获取数字类型字段
+               and not field.primary_key  # 排除主键
+               and not isinstance(field, ForeignKey)  # 排除外键
+               and field.name != 'HardwareInfo'  # 排除特定字段
+        ]
+        return field_names
 
-    # extract needed fields
-    # exclude PK, FK, field for DeviceName
-    field_names = [
-        field.name for field in fields
-        if not isinstance(field, ForeignKey)
-           and not field.primary_key
-           and field.name != 'HardwareInfo'
-    ]
+    # 获取每个模型的数字类型字段
+    task_results_fields = extract_numeric_fields(TaskResults)
+    total_results_fields = extract_numeric_fields(TotalResults)
+    task_graph_results_fields = extract_numeric_fields(TaskGraphResults)
 
-    return json.dumps(field_names)
+    # 合并所有字段名到一个列表中
+    all_fields = task_results_fields + total_results_fields + task_graph_results_fields
+
+    return json.dumps(all_fields)
+
 
 
 def get_all_run_ids():
@@ -177,32 +187,62 @@ def load_customize_chart_datas(x_axes, y_axes):
 
 def load_compared_paired_chart_data(comparison_mode, parameter_type,
                                     run_ids, device_names, benchmark_name, max_data_size=100):
+    """
+    Get chart data from database
+    :param comparison_mode: byRun or byDevice
+    :param parameter_type: the data that wants to visualize
+    :param run_ids:
+    :param device_names:
+    :param benchmark_name:
+    :param max_data_size:
+    :return:
+    """
     data_pack = {}
+
     if comparison_mode == 'byRun':
         for run_id in run_ids:
-
             # Get specific run table
             run = Run.objects.get(RunID=run_id)
 
             # Get specific bm with same run and specific bm name
             benchmarks = Benchmark.objects.filter(Run=run, BenchmarkName=benchmark_name)
 
-            # Get Benchmark
+            # Get TotalResults
             total_results = TotalResults.objects.filter(Benchmark__in=benchmarks)
 
-            # Get TotalResults
+            # Get TaskGraphResults
             task_graph_results = TaskGraphResults.objects.filter(Result__in=total_results)
 
-            # Get TaskGraphResults
+            # Get TaskResults
             task_results = TaskResults.objects.filter(TaskGraphResult__in=task_graph_results)
 
-            # TODO can fit with other param types that in other tables
-            datas = task_results.values('HardwareInfo', parameter_type)[:max_data_size]
+            # Determine which table contains the parameter_type
+            if parameter_type in [field.name for field in TaskResults._meta.fields]:
+                datas = task_results.values('HardwareInfo', parameter_type)[:max_data_size]
+                chart_data = [['HardwareInfo', parameter_type]]
+            elif parameter_type in [field.name for field in TaskGraphResults._meta.fields]:
+                # Map data to ('HardwareInfo', param_type) format
+                mapped_datas = task_results.values('HardwareInfo', 'TaskGraphResult__' + parameter_type)[:max_data_size]
+                chart_data = [['HardwareInfo', parameter_type]]
+                datas = [
+                    {'HardwareInfo': entry['HardwareInfo'], parameter_type: entry['TaskGraphResult__' + parameter_type]}
+                    for entry in mapped_datas]
+            elif parameter_type in [field.name for field in TotalResults._meta.fields]:
+                # Map data to ('HardwareInfo', param_type) format
+                mapped_datas = task_results.values('HardwareInfo', 'TaskGraphResult__Result__' + parameter_type)[
+                               :max_data_size]
+                chart_data = [['HardwareInfo', parameter_type]]
+                datas = [{'HardwareInfo': entry['HardwareInfo'],
+                          parameter_type: entry['TaskGraphResult__Result__' + parameter_type]} for entry in
+                         mapped_datas]
+            else:
+                raise ValueError(f"Unknown parameter type: {parameter_type}")
 
-            chart_data = [['Device Name', parameter_type]]
             for entry in datas:
                 chart_data.append([entry['HardwareInfo'], entry[parameter_type]])
             data_pack[run_id] = chart_data
+
+    # By Device
     elif comparison_mode == 'byDevice':
         for device_name in device_names:
             # Get TaskResults with specific HardwareInfo (device name)
@@ -223,16 +263,41 @@ def load_compared_paired_chart_data(comparison_mode, parameter_type,
             # Get Runs from Benchmarks
             runs = Run.objects.filter(RunID__in=benchmarks.values_list('Run_id', flat=True))
 
-            datas = task_results.values('TaskGraphResult__Result__Benchmark__Run__RunID', parameter_type)[
-                    :max_data_size]
+            # Determine which table contains the parameter_type
+            if parameter_type in [field.name for field in TaskResults._meta.fields]:
+                datas = task_results.values('TaskGraphResult__Result__Benchmark__Run__RunID', parameter_type)[
+                        :max_data_size]
+                chart_data = [['RunID', parameter_type]]
+            elif parameter_type in [field.name for field in TaskGraphResults._meta.fields]:
+                datas = task_graph_results.values('Result__Benchmark__Run__RunID', parameter_type)[:max_data_size]
+                # Map data to ('RunID', param_type) format
+                mapped_datas = task_results.values('TaskGraphResult__Result__Benchmark__Run__RunID',
+                                                   'TaskGraphResult__' + parameter_type)[:max_data_size]
+                chart_data = [['RunID', parameter_type]]
+                datas = [{'TaskGraphResult__Result__Benchmark__Run__RunID': entry[
+                    'TaskGraphResult__Result__Benchmark__Run__RunID'],
+                          parameter_type: entry['TaskGraphResult__' + parameter_type]} for entry in mapped_datas]
+            elif parameter_type in [field.name for field in TotalResults._meta.fields]:
+                datas = total_results.values('Benchmark__Run__RunID', parameter_type)[:max_data_size]
+                # Map data to ('RunID', param_type) format
+                mapped_datas = task_results.values('TaskGraphResult__Result__Benchmark__Run__RunID',
+                                                   'TaskGraphResult__Result__' + parameter_type)[:max_data_size]
+                chart_data = [['RunID', parameter_type]]
+                datas = [{'TaskGraphResult__Result__Benchmark__Run__RunID': entry[
+                    'TaskGraphResult__Result__Benchmark__Run__RunID'],
+                          parameter_type: entry['TaskGraphResult__Result__' + parameter_type]} for entry in
+                         mapped_datas]
+            else:
+                raise ValueError(f"Unknown parameter type: {parameter_type}")
 
-            chart_data = [['Run ID', parameter_type]]
             for entry in datas:
                 chart_data.append([entry['TaskGraphResult__Result__Benchmark__Run__RunID'], entry[parameter_type]])
             data_pack[device_name] = chart_data
 
     serialized_data_pack = json.dumps(data_pack)
     return serialized_data_pack
+
+
 
 
 def get_model_by_field_name(field_name):
